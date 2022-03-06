550f877500e432e831f4e8ac98f3a67438270e0f
Sydney Creel <sydney.creel@outlook.com>
2022-03-05 23:25:36 -0800

    remove node modules

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware.
-app.use(express.urlencoded({ extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request(Not Found) Catch all
+// Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-// Start express.js server on port 3001.
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
